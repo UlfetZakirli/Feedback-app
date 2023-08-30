12ce89733bc37213ef0b17973438583f946aa386
UlfetZakirli <ulfetzakirli@gmail.com>
2023-08-30 23:38:09 +0400

    fead: real-time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,20 @@ import Button from './shared/Button'
 
 const FeedbackForm = () => {
     const [text, setText] = useState("")
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if (text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10) {
+            setMessage('Text must be at least 10 characters')
+            setBtnDisabled(true)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
 
@@ -20,8 +32,9 @@ const FeedbackForm = () => {
                         onChange={handleTextChange}
                         value={text}
                     />
-                    <Button version='secondary' type="submit">Send</Button>
+                    <Button isDisabled={btnDisabled} version='secondary' type="submit">Send</Button>
                 </div>
+                {message && <div className='message'>{message}</div>}
             </form>
         </Card>
     )
