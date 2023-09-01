5cc84f2984c9b59ba3605ac333c8f8b49289fb50
UlfetZakirli <ulfetzakirli@gmail.com>
2023-09-01 22:32:14 +0400

    feat: About page and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import { v4 as uuid } from 'uuid'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import AboutPage from './pages/AboutPage'
 
 const App = () => {
   const [feedbacks, setFeedbacks] = useState(FeedbackData)
@@ -23,12 +25,22 @@ const App = () => {
 
   return (
     <>
-      <Header />
-      <div className='container'>
-        <FeedbackForm handleAddFeedback={addFeedback} />
-        <FeedbackStats feedbacks={feedbacks} />
-        <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
-      </div>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route  path='/' element={
+              <>
+                <FeedbackForm handleAddFeedback={addFeedback} />
+                <FeedbackStats feedbacks={feedbacks} />
+                <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
+              </>
+            } />
+            <Route path='/about' element={<AboutPage />} />
+
+          </Routes>
+        </div>
+      </Router >
     </>
   )
 }
